19db37adf84986063c7eb443e4f5dba29d156f71
Maryna Demchenko <cafeine25@gmail.com>
2016-10-27 14:39:35 +0200

    commit before 3rd container

--- gruntfile.js
+++ gruntfile.js
@@ -10,13 +10,29 @@ module.exports = function(grunt) {
         'dist/main.css': 'src/sass/main.scss'       // 'destination': 'source' 
       }
     }
-  } 
+  },
+  watch: {
+  scripts: {
+    files: ['**/*.scss'],
+    tasks: ['sass'],
+    options: {
+      spawn: false,
+    },
+  },
+} 
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
   grunt.registerTask('default', ['sass']);
+  grunt.registerTask('watch', ['watch']);
 
 };
+
+
+
+
+
